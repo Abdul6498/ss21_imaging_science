afedd97c46efc0d6077f059f98111cc8c48eb7dc
sieumap43 <angiatrieu2905@gmail.com>
2021-06-22 19:14:28 +0200

    finished task 4

---
+++ assignment4/Ex_4_an/README.txt
@@ -0,0 +1,4 @@
+Abdul Rehman (3440146) M.Sc. INFOTECH
+An Trieu (3523966) M.Sc. INFOTECH
+Khanh Quynh Nguyen (3517671) M.Sc. INFOTECH
+Valentina Roldan (3519666) M.Sc. INFOTECH
---
+++ assignment4/Ex_4_an/run.cmd
@@ -0,0 +1,9 @@
+REM canny <input.pgm> <output.pgm> sigma T1 T2
+canny objects.pgm objects_output.pgm 3 2 3
+objects_output.pgm
+
+canny pruebab1.pgm pruebab1_output.pgm 3 2 3
+pruebab1_output.pgm
+
+canny peppers.pgm peppers_output.pgm 3 2 3
+peppers_output.pgm
