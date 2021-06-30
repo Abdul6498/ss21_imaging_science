e59e470e2b7e72bf1b2b18b123654a2b5aa36c98
sieumap43 <angiatrieu2905@gmail.com>
2021-06-30 18:39:04 +0200

    minor changes

--- assignment4/Ex_4_an/run.cmd
+++ assignment4/Ex_4_an/run.cmd
@@ -2,8 +2,8 @@ REM canny <input.pgm> <output.pgm> sigma T1 T2
 canny objects.pgm objects_output.pgm 3 2 3
 objects_output.pgm
 
-canny pruebab1.pgm pruebab1_output.pgm 3 2 3
+canny pruebab1.pgm pruebab1_output.pgm 5 2 3
 pruebab1_output.pgm
 
-canny peppers.pgm peppers_output.pgm 3 2 3
+canny peppers.pgm peppers_output.pgm 3 3 4
 peppers_output.pgm
